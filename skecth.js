17353377eb795a884255bf9fc52c98ce84f57e1a
AndrewNyrivera <34843760+AndrewNyrivera@users.noreply.github.com>
2019-11-29 22:03:38 -0500

    Update skecth.js

--- skecth.js
+++ skecth.js
@@ -7,7 +7,7 @@ function preload() {
     //img of spaceship
     img = loadImage("https://raw.githubusercontent.com/AndrewNyrivera/spaceinvader/master/babyyoda.png");
     //Alien 1
-    img1 = loadImage("https://raw.githubusercontent.com/AndrewNyrivera/spaceinvader/master/stormtrooper.jpg");
+    img1 = loadImage("https://raw.githubusercontent.com/AndrewNyrivera/spaceinvader/master/alien1.png");
     //Don't forget about alien 2 and set up a server
 
     //explosion image
